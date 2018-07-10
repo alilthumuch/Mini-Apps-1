9b14c7cffc90c5f11af0ff0e068352b3ae92653f
alilthumuch <alilthumuch@gmail.com>
2018-07-10 16:56:57 -0700

    Server get/post working

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,68 @@
+var app = {
+	server: 'http://localhost:3000/',
+
+	init: function(){
+		app.$message = $('#message');
+		
+		app.$textData = $('#textData');
+
+		app.$submitButton = document.getElementById('submitbutton')
+
+		//app.$message.on('submit', app.retrieveData); //event listener
+
+		app.$submitButton.addEventListener('click', app.retrieveData)
+	},
+
+	fetch: function () {
+		$.ajax({
+			url: app.server + 'testing',
+			type: 'GET',
+			success: function(data){
+				console.log(data)
+				app.renderData(data);
+			},
+			error: function(error){
+				console.error("something wrong with GET req")
+				// app.RenderData(data);
+			},
+		});
+	},
+
+	send: function(message){
+		var message = JSON.parse(message)
+		$.ajax({
+			url: app.server, //how do you route get request to different url?
+			type: "POST",
+			dataType:'json',
+			data: JSON.stringify(message), //need to send things in strings
+			contentType: 'application/json',
+			success: function(data) {
+				app.fetch()
+			},
+			error: function(error) {
+				console.log("something wrong with POST Req")
+			},
+		})
+	},
+
+	retrieveData: function(event) {
+		// console.log(event)
+		// console.log(app.$textData.val())
+		event.preventDefault(); //stop from re-loading
+
+		app.send(app.$textData.val())
+		//app.$message.html(''); //clear data
+	},
+
+	renderData: function (data) {
+		var dataArr = [];
+		
+
+	}
+
+};
+
+//app.init();
+
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,43 @@
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+var data = {};
+//'../client' == __dirname
+app.use(express.static(path.join(__dirname, 'client'))); //set static file path
+
+//parse the data, chunk it up for you and append a body element to request.
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/' + 'index.html')
+})
+app.get('/testing', function (req, res) {
+  res.json(data) //stringify data and send it.
+})
+
+app.post('/', function(req,res) {
+	var recurseData = function(data) {
+		var innerArr = [];
+		for (var key in data) {
+			if(key !== "children"){
+			innerArr.push(data[key])	
+			}
+			
+		}
+	}
+
+	recurseData(req.body)
+
+	data = req.body;
+	res.status(201).send(req.body);
+})
+
+app.listen(3000, () => {
+	console.log("We are live!");
+})
 
